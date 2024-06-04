bffbaa923c07f6f70c897e77c596f78b2c62a642
Ajit kumar mohapatra <167737649+Ajitkumarmohapatra@users.noreply.github.com>
2024-06-04 10:50:13 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,11 @@
+class test
+{
+    public static void main(String[] args) {
+        String s1 ="Ajit";
+
+        System.out.println(s1);
+
+        String s2 = new String("Ajit");
+        System.out.println(s2);
+    }
+}
