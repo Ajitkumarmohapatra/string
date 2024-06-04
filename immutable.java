4b3734ef5e58eb99526f2b75af30ab22c94f6752
Ajit kumar mohapatra <167737649+Ajitkumarmohapatra@users.noreply.github.com>
2024-06-04 10:53:54 +0530

    Create immutable.java

---
+++ immutable.java
@@ -0,0 +1,9 @@
+ class immutable {
+    public static void main(String[] args) {
+        String s ="sachin";
+        String s1= s.concat(" Tendulkar");
+        System.out.println(s);
+
+        System.out.println(s1);
+    }
+}
